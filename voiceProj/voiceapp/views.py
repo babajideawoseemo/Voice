8c6f814ba2e6b75ec59afac1a0b0c5b3f867c5e7
akeens4 <akinloluolaposi@gmail.com>
2019-11-12 11:33:12 +0100

    Additional Files added

--- voiceProj/voiceapp/views.py
+++ voiceProj/voiceapp/views.py
@@ -1,5 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
+from voiceapp.models import Post
+
+
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'voiceapp/post_list.html',{})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'voiceapp/post_list.html',{'posts':posts})
+
+def post_detail(request,pk):
+    post = get_object_or_404(Post,pk=pk)
+    return render(request, 'voiceapp/post_detail.html',{'post':post})
+
+    
